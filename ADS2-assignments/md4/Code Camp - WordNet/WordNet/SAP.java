92b5f86706076afa41317d57e0eb481bf3a7d77c
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-11-04 11:09:51 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-assignments/md4/Code Camp - WordNet/WordNet/SAP.java
+++ ADS2-assignments/md4/Code Camp - WordNet/WordNet/SAP.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
+/**
+ *the class to find the shortest.
+ *ancestor path.
+ */
 public class SAP {
+    /**
+     *the graph object.
+     */
     Digraph graph;
-    // constructor takes a digraph (not necessarily a DAG)
-    public SAP(Digraph G) {
-        graph = G;
+    /**
+     *constructor is to initialize objects
+     *
+     * @param     g    graph object
+     */
+    public SAP(Digraph g) {
+        graph = g;
     }
-
-    // // length of shortest ancestral path between v and w; -1 if no such path
-    // public int length(int v, int w)
-
-    // // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    // public int ancestor(int v, int w)
-
-    // // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int[] length(ArrayList<Integer> listOne, ArrayList<Integer> listTwo) {
         int min = graph.V();
         int tempOne = 0;
@@ -34,10 +37,4 @@ public class SAP {
     int[] result = {min, tempOne};
     return result;
         }
-
-    // // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    // public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
-
-    // // do unit testing of this class
-    // public static void main(String[] args)
 }
