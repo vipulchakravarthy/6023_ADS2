ebf52056f7e7b26e99a235f7bf1e6beba53badf7
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-11-04 14:30:56 +0530

    "ads2_m6_a1_1: 5/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-assignments/md6/ADS-2Week-1/Solution.java
+++ ADS2-assignments/md6/ADS-2Week-1/Solution.java
@@ -76,9 +76,9 @@ class PageRank {
         //      }
         //  }
         // }
-        final int thousand = 999;
+        final int thousand = 1000;
         double[] tempArray = new double[graph.vertices()];
-        for (int j = 0; j < thousand; j++) {
+        for (int j = 1; j < thousand; j++) {
             for (int i = 0; i < vertices; i++) {
                 sum = 0.0;
                 for (int each : revGraph.adj(i)) {
