17846bfcf315f65ecf41cdb6aed57e1316da8f71
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-11-06 17:59:04 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2-assignments/md8/1. Shortest Paths/Stack.java
+++ ADS2-assignments/md8/1. Shortest Paths/Stack.java
@@ -1,62 +1,31 @@
-/******************************************************************************
- *  Compilation:  javac Stack.java
- *  Execution:    java Stack < input.txt
- *  Dependencies: StdIn.java StdOut.java
- *  Data files:   http://algs4.cs.princeton.edu/13stacks/tobe.txt
- *
- *  A generic stack, implemented using a singly-linked list.
- *  Each stack element is of type Item.
- *
- *  This version uses a static nested class Node (to save 8 bytes per
- *  Node), whereas the version in the textbook uses a non-static nested
- *  class (for simplicity).
- *
- *  % more tobe.txt
- *  to be or not to - be - - that - - - is
- *
- *  % java Stack < tobe.txt
- *  to be not that or be (2 left on stack)
- *
- ******************************************************************************/
-
-// package edu.princeton.cs.algs4;
-
 import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-
 /**
- *  The {@code Stack} class represents a last-in-first-out (LIFO) stack of generic items.
- *  It supports the usual <em>push</em> and <em>pop</em> operations, along with methods
- *  for peeking at the top item, testing if the stack is empty, and iterating through
- *  the items in LIFO order.
- *  <p>
- *  This implementation uses a singly-linked list with a static nested class for
- *  linked-list nodes. See {@link LinkedStack} for the version from the
- *  textbook that uses a non-static nested class.
- *  See {@link ResizingArrayStack} for a version that uses a resizing array.
- *  The <em>push</em>, <em>pop</em>, <em>peek</em>, <em>size</em>, and <em>is-empty</em>
- *  operations all take constant time in the worst case.
- *  <p>
- *  For additional documentation,
- *  see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  @param <Item> the generic type of an item in this stack
+ * Stack class.
+ * @param      <Item>  The item
  */
 public class Stack<Item> implements Iterable<Item> {
-    private Node<Item> first;     // top of stack
-    private int n;                // size of the stack
-
-    // helper linked list class
+    /**
+     * top of stack.
+     */
+    private Node<Item> first;
+    /**
+     * size of the stack.
+     */
+    private int n;
+    /**
+     * Class for node.
+     * @param      <Item>  The item
+     */
     private static class Node<Item> {
+        /**
+         * item of type Item.
+         */
         private Item item;
+        /**
+         * next of type Node.
+         */
         private Node<Item> next;
     }
-
     /**
      * Initializes an empty stack.
      */
@@ -64,68 +33,60 @@ public class Stack<Item> implements Iterable<Item> {
         first = null;
         n = 0;
     }
-
     /**
      * Returns true if this stack is empty.
-     *
-     * @return true if this stack is empty; false otherwise
+     * @return true if this stack is empty;
+     * false otherwise
+     * Time complexity for this method is O(1).
      */
     public boolean isEmpty() {
         return first == null;
     }
-
     /**
      * Returns the number of items in this stack.
-     *
      * @return the number of items in this stack
+     * Time complexity for this method is O(1).
      */
     public int size() {
         return n;
     }
-
     /**
      * Adds the item to this stack.
-     *
      * @param  item the item to add
+     * Time complexity for this method is O(1).
      */
-    public void push(Item item) {
+    public void push(final Item item) {
         Node<Item> oldfirst = first;
         first = new Node<Item>();
         first.item = item;
         first.next = oldfirst;
         n++;
     }
-
     /**
      * Removes and returns the item most recently added to this stack.
-     *
      * @return the item most recently added
-     * @throws NoSuchElementException if this stack is empty
+     * Time complexity for this method is O(1).
      */
     public Item pop() {
-        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
         Item item = first.item;        // save item to return
         first = first.next;            // delete first node
         n--;
         return item;                   // return the saved item
     }
-
-
     /**
-     * Returns (but does not remove) the item most recently added to this stack.
-     *
+     * Returns (but does not remove) the item most recently
+     * added to this stack.
      * @return the item most recently added to this stack
-     * @throws NoSuchElementException if this stack is empty
+     * Time complexity for this method is O(1).
      */
     public Item peek() {
-        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
         return first.item;
     }
-
     /**
      * Returns a string representation of this stack.
-     *
-     * @return the sequence of items in this stack in LIFO order, separated by spaces
+     * @return the sequence of items in this stack in
+     * LIFO order, separated by spaces
+     * Time complexity for this method is O(N).
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
@@ -135,81 +96,55 @@ public class Stack<Item> implements Iterable<Item> {
         }
         return s.toString();
     }
-
-
     /**
-     * Returns an iterator to this stack that iterates through the items in LIFO order.
-     *
-     * @return an iterator to this stack that iterates through the items in LIFO order
+     * Returns an iterator to this stack that
+     * iterates through the items in LIFO order.
+     * @return an iterator to this stack that iterates
+     * through the items in LIFO order
      */
     public Iterator<Item> iterator() {
         return new ListIterator<Item>(first);
     }
-
-    // an iterator, doesn't implement remove() since it's optional
+    /**
+     * Class for list iterator.
+     * @param      <Item>  The item
+     */
     private class ListIterator<Item> implements Iterator<Item> {
+        /**
+         * current of type Node.
+         */
         private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
+        /**
+         * Constructs the object.
+         * @param      f  The first
+         */
+        ListIterator(final Node<Item> f) {
+            current = f;
         }
-
+        /**
+         * Determines if it has next.
+         * @return     True if has next, False otherwise.
+         * Time complexity for this method is O(1).
+         */
         public boolean hasNext() {
             return current != null;
         }
-
+        /**
+         * remove method.
+         */
         public void remove() {
             throw new UnsupportedOperationException();
         }
-
+        /**
+         * This returns the next item of the current item.
+         * @return     Item.
+         * Time complexity for this method is O(1).
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
             current = current.next;
             return item;
         }
     }
-
-
-    // /**
-    //  * Unit tests the {@code Stack} data type.
-    //  *
-    //  * @param args the command-line arguments
-    //  */
-    // public static void main(String[] args) {
-    //     Stack<String> stack = new Stack<String>();
-    //     while (!StdIn.isEmpty()) {
-    //         String item = StdIn.readString();
-    //         if (!item.equals("-"))
-    //             stack.push(item);
-    //         else if (!stack.isEmpty())
-    //             StdOut.print(stack.pop() + " ");
-    //     }
-    //     StdOut.println("(" + stack.size() + " left on stack)");
-    // }
 }
 
-
-/******************************************************************************
- *  Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
- *
- *  This file is part of algs4.jar, which accompanies the textbook
- *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
- *
- *
- *  algs4.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  algs4.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
