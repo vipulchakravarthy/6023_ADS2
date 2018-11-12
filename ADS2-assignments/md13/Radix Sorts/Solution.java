7826018b54a5558c9202609aca99692a14828b90
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-11-12 15:10:55 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2-assignments/md13/Radix Sorts/Solution.java
+++ ADS2-assignments/md13/Radix Sorts/Solution.java
@@ -8,38 +8,38 @@ class LSDSort {
     /**
      *an empty constructor.
      */
-     LSDSort() {
-     }
-     /**
-      *this method is to perform the.
-      *lsd sorting technique.
-      * @param      array        The array
-      * @param      fixedLength  The fixed length
-      *
-      * @return sorted array.
-      */
-     public String[] sort(final String[] array,
-      final int fixedLength) {
+    LSDSort() {
+    }
+    /**
+     *this method is to perform the.
+     *lsd sorting technique.
+     * @param      array        The array
+     * @param      fixedLength  The fixed length
+     *
+     * @return sorted array.
+     */
+    public String[] sort(final String[] array,
+                         final int fixedLength) {
         int length = array.length;
-        int R = 256;
+        final int radix = 256;
         String[] aux = new String[length];
-        for(int d = fixedLength - 1; d >= 0; d--) {
-            int[] count = new int[R + 1];
-            for(int i = 0; i < length; i++) {
+        for (int d = fixedLength - 1; d >= 0; d--) {
+            int[] count = new int[radix + 1];
+            for (int i = 0; i < length; i++) {
                 count[array[i].charAt(d) + 1]++;
             }
-            for(int r = 0; r < R; r++) {
+            for (int r = 0; r < radix; r++) {
                 count[r + 1] += count[r];
             }
-            for(int i = 0; i < length; i++) {
+            for (int i = 0; i < length; i++) {
                 aux[count[array[i].charAt(d)]++] = array[i];
             }
-            for(int i = 0; i < length; i++) {
+            for (int i = 0; i < length; i++) {
                 array[i] = aux[i];
             }
         }
         return array;
-     }
+    }
 }
 /**
  *class for solution.
@@ -60,12 +60,12 @@ final class Solution {
         Scanner scan = new Scanner(System.in);
         int inputs = Integer.parseInt(scan.nextLine());
         String[] rollNumbers = new String[inputs];
-        for(int i = 0; i < inputs; i++) {
+        for (int i = 0; i < inputs; i++) {
             rollNumbers[i] = scan.nextLine();
         }
         LSDSort lsdObj = new LSDSort();
         rollNumbers = lsdObj.sort(rollNumbers,
-         rollNumbers[0].length());
+                                  rollNumbers[0].length());
         System.out.println(Arrays.toString(rollNumbers));
     }
 }
