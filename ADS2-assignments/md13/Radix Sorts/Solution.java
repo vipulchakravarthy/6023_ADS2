0759157db7fa35fabd70c73c1123eecf606f2fc1
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-11-12 15:08:43 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-assignments/md13/Radix Sorts/Solution.java
+++ ADS2-assignments/md13/Radix Sorts/Solution.java
@@ -1,45 +1,71 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ *class for least significant digit.
+ *string sort.
+ */
 class LSDSort {
-	 LSDSort() {
-
-	 }
-	 public String[] sort(final String[] array, final int fixedLength) {
-	 	int length = array.length;
-	 	int R = 256;
-	 	String[] aux = new String[length];
-	 	for(int d = fixedLength - 1; d >= 0; d--) {
-	 		int[] count = new int[R + 1];
-	 		for(int i = 0; i < length; i++) {
-	 			count[array[i].charAt(d) + 1]++;
-	 		}
-	 		for(int r = 0; r < R; r++) {
-	 			count[r + 1] += count[r];
-	 		}
-	 		for(int i = 0; i < length; i++) {
-	 			aux[count[array[i].charAt(d)]++] = array[i];
-	 		}
-	 		for(int i = 0; i < length; i++) {
-	 			array[i] = aux[i];
-	 		}
-	 	}
-	 	return array;
-	 }
-
+    /**
+     *an empty constructor.
+     */
+     LSDSort() {
+     }
+     /**
+      *this method is to perform the.
+      *lsd sorting technique.
+      * @param      array        The array
+      * @param      fixedLength  The fixed length
+      *
+      * @return sorted array.
+      */
+     public String[] sort(final String[] array,
+      final int fixedLength) {
+        int length = array.length;
+        int R = 256;
+        String[] aux = new String[length];
+        for(int d = fixedLength - 1; d >= 0; d--) {
+            int[] count = new int[R + 1];
+            for(int i = 0; i < length; i++) {
+                count[array[i].charAt(d) + 1]++;
+            }
+            for(int r = 0; r < R; r++) {
+                count[r + 1] += count[r];
+            }
+            for(int i = 0; i < length; i++) {
+                aux[count[array[i].charAt(d)]++] = array[i];
+            }
+            for(int i = 0; i < length; i++) {
+                array[i] = aux[i];
+            }
+        }
+        return array;
+     }
 }
+/**
+ *class for solution.
+ */
 final class Solution {
-	private Solution() {
+    /**
+     *an empty solution.
+     */
+    private Solution() {
 
-	}
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int inputs = Integer.parseInt(scan.nextLine());
-		String[] rollNumbers = new String[inputs];
-		for(int i = 0; i < inputs; i++) {
-			rollNumbers[i] = scan.nextLine();
-		}
-		LSDSort lsdObj = new LSDSort();
-		rollNumbers = lsdObj.sort(rollNumbers, rollNumbers[0].length());
-		System.out.println(Arrays.toString(rollNumbers));
-	}
+    }
+    /**
+     *the main method for user input.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int inputs = Integer.parseInt(scan.nextLine());
+        String[] rollNumbers = new String[inputs];
+        for(int i = 0; i < inputs; i++) {
+            rollNumbers[i] = scan.nextLine();
+        }
+        LSDSort lsdObj = new LSDSort();
+        rollNumbers = lsdObj.sort(rollNumbers,
+         rollNumbers[0].length());
+        System.out.println(Arrays.toString(rollNumbers));
+    }
 }
