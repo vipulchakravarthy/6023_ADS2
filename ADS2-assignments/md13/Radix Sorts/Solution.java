482d0d8b6a2e14131cf7c5097f1c297239755842
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-11-12 15:01:59 +0530

    "ads2_m13_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS2-assignments/md13/Radix Sorts/Solution.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+class LSDSort {
+	 LSDSort() {
+
+	 }
+	 public String[] sort(final String[] array, final int fixedLength) {
+	 	int length = array.length;
+	 	int R = 256;
+	 	String[] aux = new String[length];
+	 	for(int d = fixedLength - 1; d >= 0; d--) {
+	 		int[] count = new int[R + 1];
+	 		for(int i = 0; i < length; i++) {
+	 			count[array[i].charAt(d) + 1]++;
+	 		}
+	 		for(int r = 0; r < R; r++) {
+	 			count[r + 1] += count[r];
+	 		}
+	 		for(int i = 0; i < length; i++) {
+	 			aux[count[array[i].charAt(d)]++] = array[i];
+	 		}
+	 		for(int i = 0; i < length; i++) {
+	 			array[i] = aux[i];
+	 		}
+	 	}
+	 	return array;
+	 }
+
+}
+final class Solution {
+	private Solution() {
+
+	}
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int inputs = Integer.parseInt(scan.nextLine());
+		String[] rollNumbers = new String[inputs];
+		for(int i = 0; i < inputs; i++) {
+			rollNumbers[i] = scan.nextLine();
+		}
+		LSDSort lsdObj = new LSDSort();
+		rollNumbers = lsdObj.sort(rollNumbers, rollNumbers[0].length());
+		for(int i = 0; i < inputs; i++) {
+			System.out.println(rollNumbers[i]);
+		}
+	}
+}
