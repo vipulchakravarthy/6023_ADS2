e43d58c92a805dd097772d3c42178b13c2a26b71
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-11-16 09:24:10 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-assignments/md16/boggle/Solution.java
+++ ADS2-assignments/md16/boggle/Solution.java
@@ -3,57 +3,54 @@ import java.util.Arrays;
  * Class for solution.
  */
 public class Solution {
-
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		// empty constructor
-	}
-
-	/**
-	 * Main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		String caseType = StdIn.readLine();
-		switch (caseType) {
-		case "Score":
-			String dictionaryName = StdIn.readLine();
-			In in = new In("/Files/" + dictionaryName);
-			String[] dictionary = in.readAllStrings();
-			String boardName = StdIn.readLine();
-			BoggleBoard board = new BoggleBoard("/Files/" + boardName);
-			BoggleSolver solver = new BoggleSolver(dictionary);
-			int score = 0;
-			for (String word : solver.getAllValidWords(board)) {
-				score += solver.scoreOf(word);
-			}
-			StdOut.println("Score = " + score);
-			break;
-
-		default:
-			try {
-				dictionaryName = StdIn.readLine();
-				in = new In("/Files/" + dictionaryName);
-				dictionary = in.readAllStrings();
-				board = null;
-				if(board == null) {
-					System.out.println("board is null");
-					return;
-				}
-				solver = new BoggleSolver(dictionary);
-				score = 0;
-				for (String word : solver.getAllValidWords(board)) {
-					score += solver.scoreOf(word);
-				}
-				StdOut.println("Score = " + score);
-			} catch (Exception ex) {
-				System.out.println(ex.getMessage());
-			}
-			break;
-		}
-
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // empty constructor
+    }
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String caseType = StdIn.readLine();
+        switch (caseType) {
+        case "Score":
+            String dictionaryName = StdIn.readLine();
+            In in = new In("/Files/" + dictionaryName);
+            String[] dictionary = in.readAllStrings();
+            String boardName = StdIn.readLine();
+            BoggleBoard board = new BoggleBoard(
+                "/Files/" + boardName);
+            BoggleSolver solver = new BoggleSolver(dictionary);
+            int score = 0;
+            for (String word : solver.getAllValidWords(board)) {
+                score += solver.scoreOf(word);
+            }
+            StdOut.println("Score = " + score);
+            break;
+        default:
+            try {
+                dictionaryName = StdIn.readLine();
+                in = new In("/Files/" + dictionaryName);
+                dictionary = in.readAllStrings();
+                board = null;
+                if(board == null) {
+                    System.out.println("board is null");
+                    return;
+                }
+                solver = new BoggleSolver(dictionary);
+                score = 0;
+                for (String word : solver.getAllValidWords(board)) {
+                    score += solver.scoreOf(word);
+                }
+                StdOut.println("Score = " + score);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+            break;
+        }
+    }
 }
